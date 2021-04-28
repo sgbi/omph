8c48c3f0b89ce7e13c4a5ba7c58ba2b5b275a337
krquinton <ton.quinton@gmail.com>
2021-04-28 18:44:29 +0800

    initial commit

---
+++ data.js
@@ -0,0 +1,291 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-omph-front-left",
+      "name": "OMPH Front-Left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3504104260447196,
+        "pitch": 0.3814388902773178,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.2537621221392925,
+          "pitch": 0.3226473677783215,
+          "rotation": 0,
+          "target": "2-omph-front-center"
+        },
+        {
+          "yaw": -0.28396684033746844,
+          "pitch": 0.47669514209251496,
+          "rotation": 0,
+          "target": "3-omph-front-center-2"
+        },
+        {
+          "yaw": -0.5191935100132756,
+          "pitch": 0.10288444889851078,
+          "rotation": 0,
+          "target": "1-omph-front-right"
+        },
+        {
+          "yaw": 0.28286886827685365,
+          "pitch": 0.06376777580156379,
+          "rotation": 0,
+          "target": "4-omph-back"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-omph-front-right",
+      "name": "OMPH Front-Right",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.32355719210048406,
+        "pitch": 0.672695279055274,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9075150486260632,
+          "pitch": 0.4758633662999028,
+          "rotation": 0,
+          "target": "3-omph-front-center-2"
+        },
+        {
+          "yaw": 0.8749252781751142,
+          "pitch": 0.37897832764793016,
+          "rotation": 0,
+          "target": "2-omph-front-center"
+        },
+        {
+          "yaw": 0.8573821491682558,
+          "pitch": 0.16861908225510547,
+          "rotation": 0,
+          "target": "0-omph-front-left"
+        },
+        {
+          "yaw": -0.5429842885837708,
+          "pitch": 0.14742837069460535,
+          "rotation": 0,
+          "target": "4-omph-back"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-omph-front-center",
+      "name": "OMPH Front-Center",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.038934267929105815,
+        "pitch": 0.6726952790552687,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.033522605955685236,
+          "pitch": 1.2350502055031836,
+          "rotation": 0,
+          "target": "3-omph-front-center-2"
+        },
+        {
+          "yaw": -0.445841657415496,
+          "pitch": 0.1186498446235813,
+          "rotation": 0,
+          "target": "4-omph-back"
+        },
+        {
+          "yaw": -1.271882446995388,
+          "pitch": 0.3061403610825053,
+          "rotation": 0,
+          "target": "0-omph-front-left"
+        },
+        {
+          "yaw": 1.2870145485961757,
+          "pitch": 0.4737036215969841,
+          "rotation": 0,
+          "target": "1-omph-front-right"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-omph-front-center-2",
+      "name": "OMPH Front-Center 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.07384617964694229,
+        "pitch": 0.8399433442478692,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0394393866727683,
+          "pitch": -1.3051071225957287,
+          "rotation": 0,
+          "target": "2-omph-front-center"
+        },
+        {
+          "yaw": 1.7966849866003258,
+          "pitch": -0.31342141122561884,
+          "rotation": 0,
+          "target": "1-omph-front-right"
+        },
+        {
+          "yaw": -1.3734714145035767,
+          "pitch": -0.24350089700853772,
+          "rotation": 0,
+          "target": "0-omph-front-left"
+        },
+        {
+          "yaw": -0.4887457939112654,
+          "pitch": -0.14767540504348986,
+          "rotation": 0,
+          "target": "4-omph-back"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-omph-back",
+      "name": "OMPH Back",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.3099290821519194,
+        "pitch": 0.7336520893161058,
+        "fov": 1.3378744097411812
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6613021599528253,
+          "pitch": 0.18139983467142784,
+          "rotation": 0,
+          "target": "1-omph-front-right"
+        },
+        {
+          "yaw": -0.2053319937499687,
+          "pitch": 0.151608087497916,
+          "rotation": 0,
+          "target": "0-omph-front-left"
+        },
+        {
+          "yaw": 0.23928612000609917,
+          "pitch": 0.2920153703994792,
+          "rotation": 0,
+          "target": "3-omph-front-center-2"
+        },
+        {
+          "yaw": 0.2571702461094567,
+          "pitch": 0.17822692385425398,
+          "rotation": 0,
+          "target": "2-omph-front-center"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Oriental Mindoro Provincial Hospital",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
